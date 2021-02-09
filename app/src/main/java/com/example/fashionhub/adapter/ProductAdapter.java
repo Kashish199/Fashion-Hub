47ea173eb8210b47d182e3cd5fea92b6df21859c
pramodanand995947 <pramodanand99@gmail.com>
2021-02-09 00:26:04 -0500

    product adapter

---
+++ app/src/main/java/com/example/fashionhub/adapter/ProductAdapter.java
@@ -0,0 +1,99 @@
+package com.example.fashionhub.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.fashionhub.ProductDetails;
+import com.example.fashionhub.R;
+import com.example.fashionhub.model.Products;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder> {
+
+    Context context;
+    List<Products> productsList;
+
+
+    public ProductAdapter(Context context, List<Products> productsList) {
+        this.context = context;
+        this.productsList = productsList;
+    }
+
+    @NonNull
+    @Override
+    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(context).inflate(R.layout.products_row_item, parent, false);
+        return new ProductViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ProductViewHolder holder, final int position) {
+
+        Picasso.get().load(productsList.get(position).getImageUrl()).into(holder.prodImage);
+        holder.prodName.setText(productsList.get(position).getProductName());
+        holder.prodQty.setText(productsList.get(position).getProductQty());
+        holder.prodPrice.setText(productsList.get(position).getProductPrice() + " $");
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(context, ProductDetails.class);
+                Bundle b = new Bundle();
+                String id = productsList.get(position).getProductid();
+                String name = productsList.get(position).getProductName();
+                String description = productsList.get(position).getProductDescription();
+                String category = productsList.get(position).getProductCategory();
+                String price = productsList.get(position).getProductPrice();
+                Uri image = productsList.get(position).getDetailmageUrl();
+
+                b.putString("Name", name);
+                b.putString("Description", description);
+                b.putString("Category", category);
+                b.putString("Price", price);
+                b.putString("id", id);
+                b.putString("Image", String.valueOf(image));
+
+
+                i.putExtras(b);
+                context.startActivity(i/*, activityOptions.toBundle()*/);
+            }
+        });
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return productsList.size();
+    }
+
+    public static final class ProductViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView prodImage;
+        TextView prodName, prodQty, prodPrice;
+
+        public ProductViewHolder(@NonNull View itemView) {
+            super(itemView);
+            prodImage = itemView.findViewById(R.id.prod_image);
+            prodName = itemView.findViewById(R.id.prod_name);
+            prodPrice = itemView.findViewById(R.id.prod_price);
+            prodQty = itemView.findViewById(R.id.prod_qty);
+
+
+        }
+    }
+
+}
